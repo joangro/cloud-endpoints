beb54b41cfe23f0b2487b0abe3d9f68ccd675b44
joangro <jgraunoel@gmail.com>
2019-02-19 11:45:43 +0100

    added kubernetes

---
+++ kubernetes/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/projects', methods=['POST', 'GET'])
+def projects():
+    from google.cloud import resource_manager
+    client = resource_manager.Client()
+    projects = client.list_projects()
+    return '<br /> '.join(e.project_id for e in projects)
+
+
+@app.route('/blobs', methods=['POST', 'GET'])
+def bucket_files():
+    from google.cloud import storage
+    client = storage.Client()
+    bucket = client.get_bucket("buckete")
+    blobs = list(bucket.list_blobs())
+    return "<br />".join(e.name for e in blobs)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8080, debug=True)
