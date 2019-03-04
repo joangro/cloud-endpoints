e5cbd6fdc5ac9453c5dcd5721e1fbd6477edf1dd
joangro <jgraunoel@gmail.com>
2019-03-04 17:42:44 +0100

    docker creator changes

--- base/docker/create-docker-image.sh
+++ base/docker/create-docker-image.sh
@@ -7,7 +7,7 @@ IMAGE_VERSION=""
 
 main()
 {
-	IMAGE_NAME="gcr.io/${PROJECT_ID}/endpoints-image:${IMAGE_VERSION}"
+	IMAGE_NAME="gcr.io/${PROJECT_ID}/endpoints-image${IMAGE_VERSION}"
 	echo "Creating docker image with name $IMAGE_NAME ..."
 	docker build -t $IMAGE_NAME .
 
@@ -15,33 +15,27 @@ main()
 	docker push $IMAGE_NAME
 
 	echo -e "\nDONE"
-	echo "Buit image $IMAGE_NAME"
+	echo "Built image: $IMAGE_NAME"
 	echo "Run the following command to test it locally:"
 	echo -e "\ndocker run --rm -p 8080:8080 $IMAGE_NAME \n"
 }
 
 
 
-
-
 while test $# -gt 0; do
 	case $1 in
 		-h | --help)
-			echo "options:"
+			echo "Arguments:"
 			echo "-h, --help		show help and optional arguments"
-			echo "-v, --version=VERSION	specify a version to build with docker"
+			echo "-v, --version=VERSION	REQUIRED: Specify a version to build with docker (for example: --version=v1"
 			exit 0
 			;;
 		-v)
 			shift
-			IMAGE_VERSION=$1
-			main
-			exit 0
+			IMAGE_VERSION=":$1"
 			;;
 		--version*)
-			IMAGE_VERSION=`echo $1 | sed -e 's/^[^=]*=//g'`
-			main
-			exit 0
+			IMAGE_VERSION=`echo ":$1" | sed -e 's/^[^=]*=//g'`
 			;;
 		*)
 			main
