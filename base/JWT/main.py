1ae309c60b3a80f1319a9471cc689170bd2dd327
joangro <grauj@endpoints-oa-ce.c.wave16-joan.internal>
2019-03-05 13:40:17 +0000

    added JWT for all endpoints

--- base/JWT/main.py
+++ base/JWT/main.py
@@ -1,15 +1,23 @@
-import time
+import time, sys
 import google.auth.crypt
 import google.auth.jwt
+import argparse
+from argparse import RawTextHelpFormatter
 
-
-def service_account(expiry_time=3600):
+def service_account(service, expiry_time=3600):
     iat = time.time()
     exp = iat+expiry_time
-
+    if service == "ce":
+        aud='endpoints-oa-ce.endpoints.wave16-joan.cloud.goog'
+    elif service == "ae":
+        aud="endpoints.wave16-joan.appspot.com"
+    elif service == "k8s":
+        aud="endpoints-oa-k8s.wave16-joan.appspot.com"
+    else:
+        sys.exit()
     payload={'iss': 'edit21@wave16-joan.iam.gserviceaccount.com',
              'sub': 'edit21@wave16-joan.iam.gserviceaccount.com',
-             'aud': 'endpoints.wave16-joan.appspot.com',
+             'aud': aud,
              'iat': iat,
              'exp': exp}
     
@@ -21,4 +29,10 @@ def service_account(expiry_time=3600):
     
     
 if __name__ == "__main__":
-    service_account()
+    parser = argparse.ArgumentParser(description='Create JWT tokens for endpoints services', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('service', help="Create JWT for one of the services: \n \
+                                        App Engine:\tae\n \
+                                        Compute Engine:\tce\n \
+                                        Kubernetes:\tk8s\n")
+    args = parser.parse_args()
+    service_account(args.service)
